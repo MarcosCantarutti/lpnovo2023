9104059a9fe4f7947e8e2dce7283957b7e270912
Marcos <marcossandim21@gmail.com>
2023-03-06 12:21:31 -0300

    ajustes

--- script/script.js
+++ script/script.js
@@ -1,14 +1,24 @@
+let swiperWrapper = document.querySelector('.swiper-wrapper');
+let button = document.querySelector('.swiper-button-next');
+
+button.addEventListener('click', () => {
+  console.log(swiperWrapper.style.transform);
+  swiperWrapper.style.transform = 0;
+});
+
 const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
+  setWrapperSize: true,
+  direction: 'horizontal',
+  speed: 2400,
+  parallax: true,
   pagination: {
     el: '.swiper-pagination',
   },
   mousewheel: true,
   keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true,
-    },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
